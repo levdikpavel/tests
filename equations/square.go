0045d3eeb3693750424c2dfa2d39c504dfbfd6a0
levdikpavel <levdik.pavel@gmail.com>
2022-03-14 18:21:14 +0300

    solution and tests

--- equations/square.go
+++ equations/square.go
@@ -1,5 +1,41 @@
 package equations
 
-func solveSquareEquation(a,b,c float64) []float64 {
+import (
+	"fmt"
+	"math"
+)
 
+const (
+	epsilon = 1e-5
+	maxUint = ^uint(0)
+	minUint = 0
+	maxInt  = int(maxUint >> 1)
+	minInt  = -maxInt - 1
+)
+
+func solveSquareEquation(a, b, c float64) (result []float64, err error) {
+	if inEpsilon(a, 0) {
+		err = fmt.Errorf("not a square equation")
+		return
+	}
+
+	d := b*b - 4*a*c
+	if d < 0 {
+		return
+	}
+
+	if inEpsilon(d, 0) {
+		x := -b / (2 * a)
+		result = append(result, x, x)
+		return
+	}
+
+	x1 := (-b + math.Sqrt(d)) / (2 * a)
+	x2 := (-b - math.Sqrt(d)) / (2 * a)
+	result = append(result, x1, x2)
+	return
+}
+
+func inEpsilon(a, b float64) bool {
+	return math.Abs(a-b) <= epsilon
 }
--- equations/square_test.go
+++ equations/square_test.go
@@ -1,9 +1,65 @@
 package equations
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/suite"
 )
 
+func TestSquareEquation(t *testing.T) {
+	suite.Run(t, new(SquareEquationSuite))
+}
+
 type SquareEquationSuite struct {
 	suite.Suite
+}
+
+func (s *SquareEquationSuite) TestNoSolution() {
+	roots, err := solveSquareEquation(1,0,1)
+	s.Require().NoError(err)
+	s.Require().Empty(roots)
+}
+
+func (s *SquareEquationSuite) TestRoots() {
+	roots, err := solveSquareEquation(1,0,-1)
+	s.Require().NoError(err)
+	s.Require().Len(roots, 2)
+	s.assertRoots([]float64{1, -1}, roots)
+}
+
+func (s *SquareEquationSuite) TestDoubleRoot() {
+	roots, err := solveSquareEquation(1,2,1)
+	s.Require().NoError(err)
+	s.Require().Len(roots, 2)
+	s.assertRoots([]float64{-1, -1}, roots)
+}
+
+func (s *SquareEquationSuite) TestNotSquare() {
+	roots, err := solveSquareEquation(0,1,1)
+	s.Require().Error(err)
+	s.Require().Empty(roots)
+
+	roots, err = solveSquareEquation(epsilon,1,1)
+	s.Require().Error(err)
+	s.Require().Empty(roots)
+}
+
+func (s *SquareEquationSuite) TestDoubleRootEpsilon() {
+	diff := epsilon / 4
+	roots, err := solveSquareEquation(1+diff, 2+2*diff, 1+diff)
+	s.Require().NoError(err)
+	s.Require().Len(roots, 2)
+	s.assertRoots([]float64{-1, -1}, roots)
+}
+
+func (s *SquareEquationSuite) TestMaxInt() {
+	max := float64(maxInt)
+	roots, err := solveSquareEquation(max,2*max,max)
+	s.Require().NoError(err)
+	s.Require().Len(roots, 2)
+	s.assertRoots([]float64{-1, -1}, roots)
+}
+
+func (s *SquareEquationSuite) assertRoots(expected, actual []float64) {
+	s.Require().InEpsilonSlicef(expected, actual, epsilon, "expected: %v, actual: %v", expected, actual)
 }
